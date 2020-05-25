a08c2d179fb08a1e6defffa75d6ca578e63b46bb
SalimGit <56164447+SalimGit@users.noreply.github.com>
2020-05-25 17:24:54 +0200

    Portfolio

--- Rapport de stage/js/smoothscroll.js
+++ Rapport de stage/js/smoothscroll.js
@@ -32,8 +32,3 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 // Initialize all .smoothScroll links
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
 
-
-// Material Select Initialization
-$(document).ready(function() {
-    $('.mdb-select').materialSelect();
-    });
